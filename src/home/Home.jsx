2d4f9e25aabc986b6927b1916fe3efd5363fc8b6
KARIMANSHAWKI <kariman0800@gmail.com>
2021-10-11 20:56:59 +0200

    adding pure react netflix slider

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,19 @@
-import './home.scss';
-import { Navbar } from '../components/navbar/Navbar';
-import { Featured } from '../components/featured/Featured';
+import "./home.scss";
+import { Navbar } from "../components/navbar/Navbar";
+import { Featured } from "../components/featured/Featured";
+import { List } from "../components/list/List";
 
 export const Home = () => {
-    return (
-        <div className="home">
-            <Navbar />
-           <Featured type="movie"/>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured type="movie" />
+      <List />
+      <List />
+      <List />
+      <List />
+      <List />
+      <List />
+    </div>
+  );
+};
