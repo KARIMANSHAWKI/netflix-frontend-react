a5602ffd6c3a6fe631f955dbea07d31f059a6b45
KARIMANSHAWKI <kariman0800@gmail.com>
2021-10-12 02:50:04 +0200

    add register page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,18 +2,21 @@ import "./home.scss";
 import { Navbar } from "../../components/navbar/Navbar";
 import { Featured } from "../../components/featured/Featured";
 import { List } from "../../components/list/List";
+import Register from "../register/Register";
+
 
 export const Home = () => {
   return (
     <div className="home">
-      <Navbar />
+      {/* <Navbar />
       <Featured type="movie" />
       <List />
       <List />
       <List />
       <List />
       <List />
-      <List />
+      <List /> */}
+      <Register />
     </div>
   );
 };
